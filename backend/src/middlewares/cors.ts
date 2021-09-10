7e59bc6d7b3215c87d638b59c3841fa1c9cd5d35
tenkuuninja <27910240+tenkuuninja@users.noreply.github.com>
2021-09-10 21:03:54 +0700

    fix cors

--- backend/src/middlewares/cors.ts
+++ backend/src/middlewares/cors.ts
@@ -1,9 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default function(req: Request, res: Response, next: NextFunction) {
-  console.log('abc')
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 }
