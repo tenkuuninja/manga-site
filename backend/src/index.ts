8ce7a9eb51539b91dcc474163c3ec187a98aba32
tenkuuninja <27910240+tenkuuninja@users.noreply.github.com>
2021-09-29 19:55:30 +0700

    backend/update port heroku

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import cors from './middlewares/cors';
 require('dotenv').config()
 
 const app = express();
-const PORT = process.env.APP_PORT || 5000;
+const PORT = process.env.APP_PORT || process.env.PORT || 5000;
 
 sequelize.authenticate({ logging: false })
   .then(() => console.log('Connection has been established successfully.'))
