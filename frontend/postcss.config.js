f47724177c814eb394533ac70779d819c13023c9
tenkuuninja <27910240+tenkuuninja@users.noreply.github.com>
2021-09-30 01:09:03 +0700

    add plugins purgecss

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,21 @@
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ]
+let plugins = [
+  require('tailwindcss'),
+  require('autoprefixer'),
+  require('cssnano')({ preset: 'default' }),
+]
+
+if (process.env.RAILS_ENV === "production") {
+  plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './layouts/**/*.html', 
+        './src/**/*.vue', 
+        './src/**/*.jsx', 
+        './src/**/*.tsx'
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  );
 }
+
+module.exports = { plugins }
