ea4b62fb74a46de2f93b5a9e332c2cae4e10021a
tenkuuninja <27910240+tenkuuninja@users.noreply.github.com>
2021-10-18 10:38:13 +0700

    update cors

--- backend/src/middlewares/cors.ts
+++ backend/src/middlewares/cors.ts
@@ -1,7 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 
+const ACCESS_CONTROL_ALLOW_ORIGIN = process.env.ACCESS_CONTROL_ALLOW_ORIGIN?.split(',').map((host: string) => host.trim()) || '*'
+
 export default function(req: Request, res: Response, next: NextFunction) {
-  res.header("Access-Control-Allow-Origin", "*");
+  const HOST = req.headers.host || '';
+  if (ACCESS_CONTROL_ALLOW_ORIGIN === '*' || ACCESS_CONTROL_ALLOW_ORIGIN.includes('*')) {
+    res.header("Access-Control-Allow-Origin", "*");
+  } else if (ACCESS_CONTROL_ALLOW_ORIGIN.includes(HOST)) {
+    res.header("Access-Control-Allow-Origin", HOST);
+  }
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept, Authorization");
   if (req.method === 'OPTIONS') {
